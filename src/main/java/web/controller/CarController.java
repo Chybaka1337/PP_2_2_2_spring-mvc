93a879336679d9bc4c28648dc51b5db3cc14b321
Andrey Zommer <zommer.02@mail.ru>
2024-08-18 21:52:07 +0300

    add support  fornull valueÑs

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -7,15 +7,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 
+import java.util.Optional;
+
 @Controller
 public class CarController {
     @Autowired
     private CarService carService;
 
     @GetMapping("/cars")
-    public String cars(@RequestParam(value = "count") int count,
-            Model model) {
-        model.addAttribute("cars", carService.getCars(count));
+    public String cars(@RequestParam(value = "count", required = false) Optional<Integer> count,
+                       Model model) {
+        model.addAttribute("cars", carService.getCars(count.orElse(0)));
         return "cars";
     }
 }
